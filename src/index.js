b2ba570440695410348d2376fe06c636fbb0961c
Kenny Inthirath <kenny.inthirath@gmail.com>
2017-07-17 04:38:08 -0700

    Changing API endpoint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let el = document.getElementById('hello')
 
-fetch('http://localhost:8080/hello-jenkins-server/hello')
+fetch('api/hello')
     .then(res => res.json())
     .then(body => el.text = body.message)
